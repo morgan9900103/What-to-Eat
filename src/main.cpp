456227c4061080b2f9035ac4d7daa6cfdf8ea668
Morgan Lee <morgan9900103@gmail.com>
2023-04-30 02:07:20 +0000

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <time.h>
+#include <cstdlib>
+
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cout << "Please provide restaurant list" << std::endl;
+    return -1;
+  }
+  
+  // Read file
+  std::ifstream input_file;
+  input_file.open(std::string(argv[1]));
+  std::vector<std::string> restaurants;
+  if (input_file.is_open()) {
+    std::string line;
+    while (input_file.good()) {
+      std::getline(input_file, line);
+      restaurants.emplace_back(line);
+    }
+  }
+
+  // Init rand()
+  srand(time(0));
+
+  std::cout << restaurants.at(rand() % restaurants.size()) << std::endl;
+
+  return 0;
+}
